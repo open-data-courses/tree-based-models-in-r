139b6413679962f84e530c337a74d6051236f18e
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-10-03 16:22:21 -0300

    + Gaby's photo
    line 7 was missing

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import SEO from './seo'
 import { Link } from './link'
 import Logo from '../../static/logo.svg'
-
+import { H3 } from './typography'
 import '../styles/index.sass'
 import classes from '../styles/layout.module.sass'
 
