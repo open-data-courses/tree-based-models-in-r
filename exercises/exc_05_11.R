f8a87d90171cda22b4325212c591a2f3ed0d259c
Florencia D'Andrea <florencia.dandrea@gmail.com>
2020-01-15 15:02:45 -0300

    Update exc_05_11.R

--- exercises/exc_05_11.R
+++ exercises/exc_05_11.R
@@ -1,27 +1,3 @@
-# code to generate the preds objects
-#library(rpart)
-#library(ipred)
-#library(randomForest)
-#library(gbm)
-
-#load("data/credit_ch1_ex8_model.RData")
-#dt_model <- credit_model
-#load("data/credit_ch3_ex3_model.RData")
-#bag_model <- credit_model
-#load("data/credit_ch4_ex3_model.RData")
-#rf_model <- credit_model
-#load("data/credit_ch5_ex3_model.RData")
-#gbm_model <- credit_model
-#load("data/credit_ch5_ex8_ntrees.RData")
-#load("data/credit_ch5_train_test.RData")
-
-## Moved this to pre-exercise code and saved preds in RData file
-#dt_preds <- predict(dt_model, credit_test)[,"yes"]
-#bag_preds <- predict(bag_model, credit_test, type = "prob")[,"yes"]
-#rf_preds <- predict(rf_model, credit_test, type = "prob")[,"yes"]
-#gbm_preds <- predict(gbm_model, credit_test, n.trees = ntree_opt_cv)
-#save(dt_preds, bag_preds, rf_preds, gbm_preds, file = "credit_all_preds.RData")
-
 library(Metrics)
 
 load("data/credit_all_preds.RData")
@@ -30,9 +6,9 @@ load("data/credit_ch5_train_test.RData")
 # Generate the test set AUCs using the two sets of predictions & compare
 actual <- credit_test$default
 dt_auc <- auc(actual = actual, predicted = dt_preds)
-bag_auc <- auc(actual = actual, predicted = bag_preds)
-rf_auc <- auc(actual = actual, predicted = rf_preds)
-gbm_auc <- auc(actual = actual, predicted = gbm_preds)
+bag_auc <- auc(actual = actual, predicted = ___)
+rf_auc <- auc(actual = actual, predicted = ___)
+gbm_auc <- auc(actual = actual, predicted = ___)
 
 # Print results
 sprintf("Decision Tree Test AUC: %.3f", dt_auc)
